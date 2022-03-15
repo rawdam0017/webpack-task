3ffe3443dbee90a7b22d64ea9cef1b697643463c
Rawda Mohamed <rawdam39@gmail.com>
2022-03-16 00:09:34 +0200

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const pathModule = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: pathModule.resolve(__dirname, "build"),
+    assetModuleFilename: 'images/[name][ext]'
+  },
+  module: {
+    rules: [
+      //css files
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      //images
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      //sass files
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader, "css-loader", "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ inject: 'body' }),
+    new MiniCssExtractPlugin(),
+    new CssMinimizerPlugin()
+  ],
+  optimization: {
+    minimizer: [
+      "...",
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            // Lossless optimization with custom option
+            // Feel free to experiment with options for better result for you
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["mozjpeg", { quality: 60 }],
+              ["optipng", { optimizationLevel: 5 }],
+              // Svgo configuration here https://github.com/svg/svgo#configuration
+              [
+                "svgo",
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      // customize plugin options
+                      convertShapeToPath: {
+                        convertArcs: true
+                      },
+                      // disable plugins
+                      convertPathData: false
+                    }
+                  }
+                }
+              ],
+            ],
+          },
+        },
+      }),
+    ],
+  }
+
+}
